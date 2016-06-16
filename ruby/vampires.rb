ec71f84492afd0bc1f0faddf42e99f509caaf478
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-16 14:40:16 -0500

    Added for loop to check for multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,91 +4,97 @@
 # benedict.schurwanz@gmail.com
 
 
+puts "Vampire Detector"
+puts "How many employees will be processed?"
+num_employees = gets.to_i
 
-## Get Input
-puts "What is your name? "
-name = gets.chomp
+for i in (1..num_employees) do
 
-puts "How many years old are you? "
-age = gets.to_i
-# if user types letters instead of numbers, age will equal 0, which is also an invalid age
-# if user types a number with decimals - 2.3 - age will get rounded down to an integer
-while age == 0		# check for invalid input
-	puts "Please type a whole number greater than 0. How many years old are you? "
+	## Get Input
+	puts "What is your name? "
+	name = gets.chomp
+
+	puts "How many years old are you? "
 	age = gets.to_i
-end
+	# if user types letters instead of numbers, age will equal 0, which is also an invalid age
+	# if user types a number with decimals - 2.3 - age will get rounded down to an integer
+	while age == 0		# check for invalid input
+		puts "Please type a whole number greater than 0. How many years old are you? "
+		age = gets.to_i
+	end
 
-puts "What year were you born? "
-year = gets.to_i
+	puts "What year were you born? "
+	year = gets.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? "
-garlic_input = gets.chomp
-# ask again if invalid input
-until ((garlic_input == "Yes") || (garlic_input == "yes")  || (garlic_input == "y") || (garlic_input == "Y") || (garlic_input == "No") || (garlic_input == "no") || (garlic_input == "N") || (garlic_input == "n"))
-		puts "Please type \"yes\" or \"no\""
-		garlic_input = gets.chomp
-end
-# convert garlic preference to boolean based on user input
-if ((garlic_input == "Yes") || (garlic_input == "yes")  || (garlic_input == "y") || (garlic_input == "Y"))
-		garlic = true;
-elsif ((garlic_input == "No") || (garlic_input == "no") || (garlic_input == "N") || (garlic_input == "n"))
-		garlic = false;
-end
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? "
+	garlic_input = gets.chomp
+	# ask again if invalid input
+	until ((garlic_input == "Yes") || (garlic_input == "yes")  || (garlic_input == "y") || (garlic_input == "Y") || (garlic_input == "No") || (garlic_input == "no") || (garlic_input == "N") || (garlic_input == "n"))
+			puts "Please type \"yes\" or \"no\""
+			garlic_input = gets.chomp
+	end
+	# convert garlic preference to boolean based on user input
+	if ((garlic_input == "Yes") || (garlic_input == "yes")  || (garlic_input == "y") || (garlic_input == "Y"))
+			garlic = true;
+	elsif ((garlic_input == "No") || (garlic_input == "no") || (garlic_input == "N") || (garlic_input == "n"))
+			garlic = false;
+	end
 
-puts "Would you like to enroll in the company's health insurance? "
-insurance_input = gets.chomp
-# ask again if invalid input
-until ((insurance_input == "Yes") || (insurance_input == "yes")  || (insurance_input == "y") || (insurance_input == "Y") || (insurance_input == "No") || (insurance_input == "no") || (insurance_input == "N") || (insurance_input == "n"))
-		puts "Please type \"yes\" or \"no\""
-		insurance_input = gets.chomp
-end
-# convert insurance preference to boolean based on user input
-if ((insurance_input == "Yes") || (insurance_input == "yes")  || (insurance_input == "y") || (insurance_input == "Y"))
-		insurance = true;
-elsif ((insurance_input == "No") || (insurance_input == "no") || (insurance_input == "N") || (insurance_input == "n"))
-		insurance = false;
-end
+	puts "Would you like to enroll in the company's health insurance? "
+	insurance_input = gets.chomp
+	# ask again if invalid input
+	until ((insurance_input == "Yes") || (insurance_input == "yes")  || (insurance_input == "y") || (insurance_input == "Y") || (insurance_input == "No") || (insurance_input == "no") || (insurance_input == "N") || (insurance_input == "n"))
+			puts "Please type \"yes\" or \"no\""
+			insurance_input = gets.chomp
+	end
+	# convert insurance preference to boolean based on user input
+	if ((insurance_input == "Yes") || (insurance_input == "yes")  || (insurance_input == "y") || (insurance_input == "Y"))
+			insurance = true;
+	elsif ((insurance_input == "No") || (insurance_input == "no") || (insurance_input == "N") || (insurance_input == "n"))
+			insurance = false;
+	end
 
 
 
-# run vampire test
-vampire = 0		# initialize variable
-#puts vampire 	# monitor changes in variable
-if (age == (2016 - year) || age == (2015 - year)) && (garlic || insurance)
-	vampire = 1
-#	puts vampire 	# monitor changes in variable 
-end
-if !(age == (2016 - year) || age == (2015 - year)) && (!garlic || !insurance)
-	vampire = 2
-#	puts vampire 	# monitor changes in variable
-end
-if !(age == (2016 - year) || age == (2015 - year)) && !garlic && !insurance
-	vampire = 3
-#	puts vampire 	# monitor changes in variable
-end
-if name == "Drake Cula" || name == "Tu Fang"
-	vampire = 4
-#	puts vampire 	# monitor changes in variable
-end
+	# run vampire test
+	vampire = 0		# initialize variable
+	#puts vampire 	# monitor changes in variable
+	if (age == (2016 - year) || age == (2015 - year)) && (garlic || insurance)
+		vampire = 1
+	#	puts vampire 	# monitor changes in variable 
+	end
+	if !(age == (2016 - year) || age == (2015 - year)) && (!garlic || !insurance)
+		vampire = 2
+	#	puts vampire 	# monitor changes in variable
+	end
+	if !(age == (2016 - year) || age == (2015 - year)) && !garlic && !insurance
+		vampire = 3
+	#	puts vampire 	# monitor changes in variable
+	end
+	if name == "Drake Cula" || name == "Tu Fang"
+		vampire = 4
+	#	puts vampire 	# monitor changes in variable
+	end
 
-# print vampire test results
-if vampire == 1
-	puts "Probably not a vampire."
-#	puts vampire 	# check final variable value
-elsif vampire == 2
-	puts "Probably a vampire."
-#	puts vampire 	# check final variable value
-elsif vampire == 3
-	puts "Almost certainly a vampire."
-#	puts vampire 	# check final variable value
-elsif vampire == 4
-	puts "Definitely a vampire."
-#	puts vampire 	# check final variable value
-else
-	puts "Results inconclusive."
-#	puts vampire 	# check final variable value
-end
+	# print vampire test results
+	if vampire == 1
+		puts "Probably not a vampire.\n"
+	#	puts vampire 	# check final variable value
+	elsif vampire == 2
+		puts "Probably a vampire.\n"
+	#	puts vampire 	# check final variable value
+	elsif vampire == 3
+		puts "Almost certainly a vampire.\n"
+	#	puts vampire 	# check final variable value
+	elsif vampire == 4
+		puts "Definitely a vampire.\n"
+	#	puts vampire 	# check final variable value
+	else
+		puts "Results inconclusive.\n"
+	#	puts vampire 	# check final variable value
+	end
 
+end
 
 
 
