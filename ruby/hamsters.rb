db9f123e952e7bb7eaea7bcb755d865c4f652844
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-16 21:20:32 -0500

    cleaned up spaces/tabs to improve readability

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,27 +1,30 @@
 
-# hamster name
-# 
+ 
 puts "What is your hamster's name?"
 hamster_name = gets.chomp
+
 puts "What is the hamster's volume level? Enter a number between 1 and 10."
 hamster_volume = gets.chomp
 volume = hamster_volume.to_i
+
 puts "What color is the hamster?"
 hamster_color = gets.chomp
 color = hamster_color.to_s
-i=0
-until i>100
+
+i = 0
+until i > 100
 	puts "Is this hamster a good candidate for adoption? Type \"yes\" or \"no\"."
 	hamster_candidate = gets.chomp
-	if hamster_candidate=="yes"
-		candidate=true
+	if hamster_candidate == "yes"
+		candidate = true
 		break
-	elsif hamster_candidate=="no"
-		candidate=false
+	elsif hamster_candidate == "no"
+		candidate = false
 		break
 	end
-	i+=1
-end		
+	i += 1
+end	
+
 puts "How old is your hamster?"
 hamster_age = gets.chomp
 if hamster_age == ""
