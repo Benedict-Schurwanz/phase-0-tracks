cea079daacee6b9cbb6ea740db17f9898b482910
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-23 14:35:17 -0500

    Added UI to get data; method for printing hash; 1st draft of method calls

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,4 @@
-# Phase 0 - 5.3
+# Phase 0 - 5.3 - Release 3
 # Benedict Schurwanz
 
 
@@ -24,5 +24,72 @@ luxury finishes?
 done
 
 =end
+
+=begin 		# logic details
+
+What tasks do we need to do: 
+	make a hash
+	get user info
+
+* 	convert user info into proper format
+	put user info into hash
+
+*	ask about updates
+
+=end
+
+# Logic
+
+def print_hash(h) 
+	h.each { |key, val|
+		puts "#{key}: #{val}"
+	}
+end
+
+
+# 	UI
+
+#get data
+puts "Interior Designerator 5000"
+puts "Please enter the following details:"
+
+interior = {}
+
+puts "Name: "
+interior[:name] = gets.chomp
+
+puts "Age: "
+Age = gets.chomp
+interior[:age] = Age.to_i
+
+puts "How many children? "
+number_children = gets.chomp
+interior[:children] = number_children.to_i
+
+puts "What theme would you like for your decor? "
+interior[:decor] = gets.chomp
+
+puts "Would you like luxury finishes? "
+luxury_pref = gets.chomp
+if luxury_pref == "yes"
+	interior[:luxury] = true
+elsif luxury_pref == "no"
+	interior[:luxury] = false
+end
+
+#print data
+print_hash(interior)
+
+#offer edit
+edit_data(interior)
+
+#print edited data
+print_hash(interior)
+
+
+
+
+
+
 
 
