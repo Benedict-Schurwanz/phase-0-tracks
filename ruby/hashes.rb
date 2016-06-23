fa3e6df906e5b591ab9aaf52abfed13f716ff21f
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-22 23:30:03 -0500

    Added pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,28 @@
+# Phase 0 - 5.3
+# Benedict Schurwanz
+
+
+
+=begin		# pseudocode
+
+# Prompt user for following information	
+name
+age
+number of children
+decor theme
+luxury finishes?
+# Add each piece of data to a hash with a symbol key matching what the user was prompted for
+	- convert age, number of children to integer
+	- convert luxury to boolean
+	- name and decor theme can be strings
+
+# print hash
+# do you want to update? which field do you want to change? Enter "none" if you are satisfied with your entries. 
+	# ignore bad input
+
+# print hash
+done
+
+=end
+
+
