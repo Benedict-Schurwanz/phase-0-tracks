f81f9b3bfa640cae346383a7e2dbb63258585f3d
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-24 16:28:48 -0500

    added coffee shop name field to hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,6 +2,7 @@
 # Benedict Schurwanz
 
 coffee_shop = {
+	coffee_shop_name: "Metropolis Coffee Company", 
 	table_1: {
 		num_people: 1, 
 		drinks: [ "iced coffee", "Red Eye Coffee" ], 
@@ -10,5 +11,5 @@ coffee_shop = {
 	table_2: {}
 }
 
-puts "at the coffee shop, at Table 1 there are #{coffee_shop[:table_1][:num_people]} people, drinking #{coffee_shop[:table_1][:drinks][0]} and #{coffee_shop[:table_1][:drinks][1]}, and using a #{coffee_shop[:table_1][:computers][0]} computer."
+puts "at the #{coffee_shop[:coffee_shop_name]} coffee shop, at Table 1 there are #{coffee_shop[:table_1][:num_people]} people, drinking #{coffee_shop[:table_1][:drinks][0]} and #{coffee_shop[:table_1][:drinks][1]}, and using a #{coffee_shop[:table_1][:computers][0]} computer."
 
