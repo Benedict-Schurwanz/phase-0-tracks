b166bf8210b26f725c4b7cf9e9d11719cd0e538a
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-24 16:10:42 -0500

    fixed syntax, changed some internal structures to arrays

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,10 +2,11 @@
 # Benedict Schurwanz
 
 coffee_shop = {
-	table1: = {
+	table1: {
 		num_people: 1, 
-		drinks: = { num_drinks: 1, drink_type: "iced coffee" }, 
-		computers: = { num_computers: 1, computer_type: "macbook pro"}
+		drinks: [ "iced coffee" ], 
+		computers: [ "macbook pro" ]
 	}
-	table2: = {}
+	table2: {}
 }
+
