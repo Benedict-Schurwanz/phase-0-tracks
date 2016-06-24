4f1ead176a398d0604831e1383b2099bb42a2af4
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-24 16:07:18 -0500

    Initial coffee_table data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,11 @@
+# Phase 0 - 5.5 Nested Data Structures
+# Benedict Schurwanz
+
+coffee_shop = {
+	table1: = {
+		num_people: 1, 
+		drinks: = { num_drinks: 1, drink_type: "iced coffee" }, 
+		computers: = { num_computers: 1, computer_type: "macbook pro"}
+	}
+	table2: = {}
+}
