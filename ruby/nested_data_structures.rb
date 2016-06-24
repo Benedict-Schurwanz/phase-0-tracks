7d806ffe54186ddca1d855fb539d9f7c165e13a2
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-24 16:26:28 -0500

    changed to manually printing each entry rather than trying to do a loop within one line of output

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,11 +4,11 @@
 coffee_shop = {
 	table_1: {
 		num_people: 1, 
-		drinks: [ "iced coffee" ], 
-		computers: [ "macbook pro" ]
+		drinks: [ "iced coffee", "Red Eye Coffee" ], 
+		computers: [ "MacBook Pro" ]
 	}, 
 	table_2: {}
 }
 
-puts "at the coffee shop, at Table 1 there are #{coffee_shop[:table_1][:num_people]} people,"
-#puts " drinking #{coffee_shop[:table_1][:drinks].each {}}"
+puts "at the coffee shop, at Table 1 there are #{coffee_shop[:table_1][:num_people]} people, drinking #{coffee_shop[:table_1][:drinks][0]} and #{coffee_shop[:table_1][:drinks][1]}, and using a #{coffee_shop[:table_1][:computers][0]} computer."
+
