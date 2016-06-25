3219410958360a55639fbcfce29e449c21af89af
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-25 11:41:05 -0500

    Figured out how to refer to the table name, which is the key whose corresponding value is a nested hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,13 +3,20 @@
 
 coffee_shop = {
 	coffee_shop_name: "Metropolis Coffee Company", 
-	table_1: {
+	:"Table 1" => {
 		num_people: 1, 
 		drinks: [ "iced coffee", "Red Eye Coffee" ], 
 		computers: [ "MacBook Pro" ]
 	}, 
-	table_2: {}
+	:"Table 2" {}
 }
 
-puts "at the #{coffee_shop[:coffee_shop_name]} coffee shop, at #{coffee_shop[]} there are #{coffee_shop[:table_1][:num_people]} people, drinking #{coffee_shop[:table_1][:drinks][0]} and #{coffee_shop[:table_1][:drinks][1]}, and using a #{coffee_shop[:table_1][:computers][0]} computer."
+puts "at the #{coffee_shop[:coffee_shop_name]} coffee shop, at #{coffee_shop.flatten[2].to_s} there are #{coffee_shop[:"Table 1"][:num_people]} people, drinking #{coffee_shop[:"Table 1"][:drinks][0]} and #{coffee_shop[:"Table 1"][:drinks][1]}, and using a #{coffee_shop[:"Table 1"][:computers][0]} computer."
+
+#trying to figure out how to print the string corresponding to the symbol instead of printing the Table 1 hash (which is the value corresponding to the "Table 1" key)
+	# coffee_shop.flatten[2].to_s
+
+# if I make the names of the tables :"Table 1" rather than :table_1 then I can more easily print the name of the table by referring to the coffee_shop hash without explicitly hardcoding it into the puts
+ 
+
 
