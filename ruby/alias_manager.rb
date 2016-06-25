392cdfa8bf885b63d57e08d13cf3e3767108d80b
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-25 13:26:38 -0500

    fleshed out basic algorithm in comments

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,6 +3,12 @@
 
 =begin 
 
-
-
+- get name
+- swap first and last name 
+	# change string to array of words, reverse it, change it back to a string
+- Change all vowels to next vowel ("aeiou") and consonants to next consonant
+	# make strings vowels = "aeiou" and consonants = "bcdfghjklmnpqrstvwxyz", and/or arrays vowels_array and consonants_array
+	# if letter is in vowels - use the method that finds if it's a member of that array/string - then get the index of vowels where the letter is, and change the letter in the string to the next letter in the array; if it's in consonants do that with consonants array
+	# if it's the last letter - 'z' or 'u' - it needs to wrap around to the first letter
 =end
+
