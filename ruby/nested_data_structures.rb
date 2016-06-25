256ffc0fb8a8856982b971c3caa0822c898683d2
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-24 20:00:04 -0500

    tweaks

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -11,5 +11,5 @@ coffee_shop = {
 	table_2: {}
 }
 
-puts "at the #{coffee_shop[:coffee_shop_name]} coffee shop, at Table 1 there are #{coffee_shop[:table_1][:num_people]} people, drinking #{coffee_shop[:table_1][:drinks][0]} and #{coffee_shop[:table_1][:drinks][1]}, and using a #{coffee_shop[:table_1][:computers][0]} computer."
+puts "at the #{coffee_shop[:coffee_shop_name]} coffee shop, at #{coffee_shop[]} there are #{coffee_shop[:table_1][:num_people]} people, drinking #{coffee_shop[:table_1][:drinks][0]} and #{coffee_shop[:table_1][:drinks][1]}, and using a #{coffee_shop[:table_1][:computers][0]} computer."
 
