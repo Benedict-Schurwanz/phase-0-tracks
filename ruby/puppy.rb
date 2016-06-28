f927d83fc0cfae60d34960a9bf4721c650c23a38
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-06-27 22:38:24 -0500

    Added Species description for puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# 6.1 - Intro to classes
+# Benedict Schurwanz - Phase 0 Chicago Salamanders
+
+=begin 
+
+SPECIES -----------------------
+Puppius Flopulous
+
+CHARACTERISTICS ---------------
+Color: Blue
+Eyebrows: Massive
+Magic skills: beginner
+
+BEHAVIOR ----------------------
+Very long floppy ears
+Waggleable eyebrows
+
+=end
