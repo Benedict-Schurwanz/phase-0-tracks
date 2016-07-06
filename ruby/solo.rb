1df6b9983d41bd77a0be1b1766f95b94b0a86487
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-05 20:57:40 -0500

    Initial pseudocode for dragon class

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+# 6.4 Solo Challenge
+# Benedict Schurwanz
+
+=begin 		Pseudocode
+
+Fire-breathing Dragons (And other dragon types, as imagined by Dungeons & Dragons)
+
+Attributes: 
+- breath type: fire, ice, electricity, etc
+- color: colors for evil, metals for good
+- alignment: good/evil, lawful/chaotic, neutral
+
+Methods:
+- breath weapon
+- cast spell 
+- talk to npc - friend or enemy?
+- change shape / transmogrify - into what form? maybe depends on who they're talking to
+
+=end
