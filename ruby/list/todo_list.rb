77a293566abebda83abac91aa7d106f1abbdc61c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-05 23:04:53 -0500

    added class methods, tested with rspec

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,15 +1,34 @@
+# 6.6 Mandatory Pair - Testing a class with rspec
+# Benedict Schurwanz
+# Ann D'Esposito
+
+
 class TodoList
-
-	def get_items 
+	def initialize(list = [])
+		@list = list
 	end
 
-	def add_item
+
+
+	def add_item(new_item)
+		@list << new_item
 	end
 
-	def delete_item
+	def delete_item(item)
+		@list.delete(item)
 	end
 
-	def retrieve_item
+
+	def get_items	# print method
+		p @list
 	end
 
+	def retrieve_item(index)
+		p @list.fetch(index)
+	end
+	
 end
+
+# changed rspec file:
+# - added require_relative
+# - changed name of "retrieves an item by index" method to "retrieve_item"
