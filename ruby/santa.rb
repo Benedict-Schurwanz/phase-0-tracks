4976207e83145901ab3f4dfe0ea2abb728d51a5c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-07 13:48:41 -0500

    added loop to create and print 10,000 santas; commented out earlier driver code since it would be covered up by 10,000 santas' attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,12 +2,15 @@
 #Benedict Schurwanz
 
 class Santa
-	def initialize(gender, ethnicity)
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+
+	def initialize(gender, ethnicity, age = 0)
 #		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = age
 	end
 
 	def speak
@@ -38,7 +41,6 @@ class Santa
 	end
 
 			# getter methods
-	attr_reader :age, :ethnicity
 #	def age
 #		@age
 #	end
@@ -48,34 +50,76 @@ class Santa
 #	end
 
 			# setter methods
-	attr_accessor :gender
+
 #	def gender=(new_gender)
 #		@gender = new_gender
 #	end
-
 end
 
 #santa = Santa.new
 #santa.speak
 #santa.eat_milk_and_cookies("snickerdoodle")
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+#santas = []
+#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+#example_genders.length.times do |i|
+#  santas << Santa.new(example_genders[i], example_ethnicities[i])
+#end
 
 #driver code - check attributes
-santas.length.times do |i|
-	santas[i].about
+#santas.length.times do |i|
+#	santas[i].about
+#end
+
+#santas[0].gender = "Other"
+#santas[0].celebrate_birthday
+#santas[0].get_mad_at("Vixen")
+#puts "age #{santas[0].age}"
+#puts "ethnicity #{santas[0].ethnicity}"
+#santas[0].about
+
+
+=begin 			Release 4 - "Build me an army worth of the North Pole"
+
+create many santas
+- random age 0-140
+- random ethnicity - ethnicity_array
+"White", "Black", "Native American or Alaska Native", "Asian", "Native Hawaiian", "Other Pacific Islander", "Albanian", "Arab", "Armenian", "Assyrian", "Azerbaijani", "Balochi", "Bamar", "Basque", "Bengali", "Berber", "Bihari", "Bosniak", "Brahui", "Bulgarian", "Catalan", "Chuvash", "Circassian", "Congolese", "Croat", "Czech", "Danish", "Dutch", "English", "Estonian", "Faroese", "Finnish", "French", "Gagauz", "Galician", "Georgian", "German", "Greek", "Gujarati", "Han Chinese", "Hindustani", "Hui", "Hungarian", "Icelander", "Irish", "Italian", "Japanese", "Javanese", "Jewish", "Kannada", "Kazakh", "Korean", "Kurdish", "Kyrgyz", "Laz", "Lebanese", "Macedonian", "Malayali", "Malay", "Manchu", "Marathi", "Mongol", "Nepali", "Norwegian", "Oromo", "Pashtun", "Persian", "Polish", "Portuguese", "Punjabi", "Romani", "Romanian", "Russian", "Scottish", "Serb", "Sindhi", "Sinhalese", "Slovak", "Slovene", "Spanish", "Sundanese", "Swedish", "Tamil", "Telugu", "Thai", "Tibetan", "Tuareg", "Turkmen", "Turkish", "Ukrainian", "Uyghur", "Vietnamese", "Volga", "Tatar", "Welsh", "Zhuang"
+
+
+- random gender - gender_array
+"Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cisgender", "Cis Female", "Cis Male", "Cis Man", "Cis Woman", "Cisgender Female", "Cisgender Male", "Cisgender Man", "Cisgender Woman", "Female to Male", "FTM", "Gender Fluid", "Gender Nonconforming", "Gender Questioning", "Gender Variant", "Genderqueer", "Intersex", "Male to Female", "MTF", "Neither", "Neutrois", "Non-binary", "Other", "Pangender", "Trans", "Trans*", "Trans Female", "Trans* Female", "Trans Male", "Trans* Male", "Trans Man", "Trans* Man", "Trans Person", "Trans* Person", "Trans Woman", "Trans* Woman", "Transfeminine", "Transgender", "Transgender Female", "Transgender Male", "Transgender Man", "Transgender Person", "Transgender Woman", "Transmasculine", "Transsexual", "Transsexual Female", "Transsexual Male", "Transsexual Man", "Transsexual Person", "Transsexual Woman", "Two-Spirit"
+
+* how to randomly select an array item:
+- array.sample
+
+* how to generate a random number:
+rand(140)
+
+=end
+
+
+ethnicity_array = ["White", "Black", "Native American or Alaska Native", "Asian", "Native Hawaiian", "Other Pacific Islander", "Albanian", "Arab", "Armenian", "Assyrian", "Azerbaijani", "Balochi", "Bamar", "Basque", "Bengali", "Berber", "Bihari", "Bosniak", "Brahui", "Bulgarian", "Catalan", "Chuvash", "Circassian", "Congolese", "Croat", "Czech", "Danish", "Dutch", "English", "Estonian", "Faroese", "Finnish", "French", "Gagauz", "Galician", "Georgian", "German", "Greek", "Gujarati", "Han Chinese", "Hindustani", "Hui", "Hungarian", "Icelander", "Irish", "Italian", "Japanese", "Javanese", "Jewish", "Kannada", "Kazakh", "Korean", "Kurdish", "Kyrgyz", "Laz", "Lebanese", "Macedonian", "Malayali", "Malay", "Manchu", "Marathi", "Mongol", "Nepali", "Norwegian", "Oromo", "Pashtun", "Persian", "Polish", "Portuguese", "Punjabi", "Romani", "Romanian", "Russian", "Scottish", "Serb", "Sindhi", "Sinhalese", "Slovak", "Slovene", "Spanish", "Sundanese", "Swedish", "Tamil", "Telugu", "Thai", "Tibetan", "Tuareg", "Turkmen", "Turkish", "Ukrainian", "Uyghur", "Vietnamese", "Volga", "Tatar", "Welsh", "Zhuang"]
+gender_array = ["Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cisgender", "Cis Female", "Cis Male", "Cis Man", "Cis Woman", "Cisgender Female", "Cisgender Male", "Cisgender Man", "Cisgender Woman", "Female to Male", "FTM", "Gender Fluid", "Gender Nonconforming", "Gender Questioning", "Gender Variant", "Genderqueer", "Intersex", "Male to Female", "MTF", "Neither", "Neutrois", "Non-binary", "Other", "Pangender", "Trans", "Trans*", "Trans Female", "Trans* Female", "Trans Male", "Trans* Male", "Trans Man", "Trans* Man", "Trans Person", "Trans* Person", "Trans Woman", "Trans* Woman", "Transfeminine", "Transgender", "Transgender Female", "Transgender Male", "Transgender Man", "Transgender Person", "Transgender Woman", "Transmasculine", "Transsexual", "Transsexual Female", "Transsexual Male", "Transsexual Man", "Transsexual Person", "Transsexual Woman", "Two-Spirit"]
+
+santas = [] 
+
+10000.times do |i|
+	random_age = rand(140)
+	random_gender = gender_array.sample
+	random_ethnicity = ethnicity_array.sample
+	santas << Santa.new(random_gender, random_ethnicity, random_age)
+	p santas[i].gender
+	p santas[i].ethnicity
+	p santas[i].age 
 end
 
-santas[0].gender = "Other"
-santas[0].celebrate_birthday
-santas[0].get_mad_at("Vixen")
-puts "age #{santas[0].age}"
-puts "ethnicity #{santas[0].ethnicity}"
-santas[0].about
+# I felt like storing it in an array - a way to do it without an array is to use just a "santa" variable and overwrite it each time through the loop
+
+
+
+
+
 
 
