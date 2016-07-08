9d3584ddb20ed4989ef0cf5587c4bc24efacdd10
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-07 21:31:14 -0500

    Rel 0: added Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,34 @@
 class Puppy
-
   def fetch(toy)
-    puts "I brought back the #{toy}!"
+    puts "I brought back the #{toy}"
     toy
   end
-
+  
+  def speak(int)
+    p "Woof! " * int
+  end
+  
+  def roll_over
+    p "*rolls over*"
+  end
+  
+  def dog_years(age)
+    p age * 7 
+  end
+  
+  def play_dead
+    p "*plays dead*"
+  end
+  
 end
+
+
+dog = Puppy.new
+
+dog.fetch("ball")
+dog.speak(5)
+dog.roll_over
+dog.dog_years(7)
+dog.play_dead
+
 
