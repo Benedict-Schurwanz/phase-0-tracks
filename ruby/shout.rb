6fdbd12e341fb8ea41b17ee9d89580b46dec5006
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-09 19:12:19 -0400

    converted Shout to mixin, added classes to use Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,7 @@
 # 6.5 Modules
 # Benedict Schurwanz
 
+=begin 			old shout module
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
@@ -13,4 +14,32 @@ end
 
 Shout.yell_angrily("loud noises")
 Shout.yelling_happily("happy noises")
+=end
 
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + "!!!" + " :D"
+	end
+end
+
+class Isley_Brothers
+	include Shout
+end
+
+class Tears_for_Fears
+	include Shout
+end
+
+tff = Tears_for_Fears.new
+ib = Isley_Brothers.new
+
+p tff.yell_angrily("Shout! Shout! Let it all out")
+p tff.yell_angrily("These are the things I can do without")
+p tff.yell_angrily("Come on, I'm talking to you")
+
+p ib.yell_happily("You know you make me wanna Shout")
