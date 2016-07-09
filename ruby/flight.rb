8a35e62b21a66947c752648cf6c8eff625fe92f7
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-09 19:01:31 -0400

    added Flight module and mixed-in to Bird and Plane

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,11 +1,25 @@
 # 6.5 Modules - Release 2
 # Benedict Schurwanz
 
-class Bird
+module Flight
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude} feet..."
+	end
+end
 
+class Bird
+	include Flight
 end
 
 class Plane
-
+	include Flight
 end
+
+
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
 
