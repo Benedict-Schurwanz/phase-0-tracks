6c896d9045988c93f93f420773f6bf5efe75d89c
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-10 02:07:30 -0400

    wrote create list method, driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -8,12 +8,12 @@
   # take the string inputed, split it into an array, use array to create a hash with the items of the array becoming keys and the default quantity (1) becoming the value
   # set default quantity - assign a variable equal to 1 for the default
   # print the list to the console - call print_list method
-# output: grocery list as hash
+# output: grocery list
 
 # Method to add an item to a list
 # input: item name and optional quantity
 # steps: use default parameter value of 1 for the quantity
-		add item to hash with quantity
+		add item to list with quantity
 # output: print out new list
 
 # Method to remove an item from the list
@@ -29,11 +29,68 @@
 
 # Method to print a list and make it look pretty
 # input: none needed
-# steps: loop through items in hash
-		inside loop, print item and quantity in a string
+# steps: LOOP through items in list
+		inside loop, print item and quantity in a sentence
 # output: printed list
 
 =end
+
+
+
+
+def create_list(initial_list)
+	groceries = {}
+	list_array = initial_list.split(' ')
+	list_array.length.times do |i|
+		groceries[list_array[i]] = 1
+	end
+	groceries
+end
+
+def add_item(item, quantity = 1)
+
+end
+
+def remove_item(item)
+
+end
+
+def update_quantity(item, new_quantity)
+
+end
+
+def print_list
+
+end
+
+
+grocery_list = create_list("carrots spaghetti bread")
+p grocery_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
