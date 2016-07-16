92876213d7212c1eae486b62900dd1a2925d8eaf
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-15 22:08:42 -0500

    added pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Javascript - Assignment 7.1
+// Benedict Schurwanz - Chi Salamanders 2016 phase 0
+
+// "reverse" function
+// - take a string
+// - take the last letter, and make it the first letter of a new string
+// - do this over and over until the first string is empty
+// - return the reversed string
+
