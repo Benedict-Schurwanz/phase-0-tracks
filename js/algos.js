2632784482bed5dd776da7f09615562088cd3240
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-16 15:26:15 -0500

    first draft of algorithm for longest string

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,34 @@
 // 7.3 Solo Challenge: Algorithm Practice
 // Benedict Schurwanz
 
+
+// psuedocode - rel 0
+
+// 		Function: takes an array of words or phrases, returns the longest word or phrase in the array
+// 
+// take an array of strings,
+// make a variable to store the index of the longest string, initialize it to 0 (which is also the first element in the array)
+// loop through the strings, do the following:
+// check the length of the current string, store that in a variable
+// if the length of the current string is longer than the length of the previous longest string, 
+// update the longest-string-index to the current index
+// 
+// after the loop, the longest string index will be that of the longest string
+// print out that string with our final index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
