64d48ff15e6da8ace32c2ce0db81861cb6736631
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-16 01:22:59 -0500

    rel 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,16 +6,31 @@
 // array of four names for horses
 
 var colors = ["blue", "black", "purple", "rainbow"];
-var horseNames = ["Mr. Ed", "Mrs. Ed", "Artax", "Black Betty"];
+var names = ["Mr. Ed", "Mrs. Ed", "Artax", "Black Betty"];
 
 // driver code
 console.log(colors); 
-console.log(horseNames);
+console.log(names);
 
 // add to each array
 colors.push("white");
-horseNames.push("Sue");
+names.push("Sue");
 
 // driver
 console.log(colors); 
-console.log(horseNames);
+console.log(names);
+
+
+// create an object with horse names and colors
+var horseys = {};
+
+for (i = 0; i < colors.length; i++){
+	horseys[names[i]] = colors[i];
+}
+
+console.log(horseys);
+
+//     attempting code to loop through object to print out 'keys' and 'values' in a user friendly output - not working out so good yet...
+// for (i = 0; i < horseys.length; i++){
+// 	console.log(Object.keys(horseys)[i]);	
+// }
