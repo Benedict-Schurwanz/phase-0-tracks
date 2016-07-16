bc1eaffde83ee7b88f8e88abb527c2666556ae01
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-07-16 00:32:31 -0500

    added arrays, added to arrays

---
+++ js/data_structures.js
@@ -0,0 +1,21 @@
+// 7.2 Data Structures
+// Benedict Schurwanz
+
+// pseudocode
+// array of four colors
+// array of four names for horses
+
+var colors = ["blue", "black", "purple", "rainbow"];
+var horseNames = ["Mr. Ed", "Mrs. Ed", "Artax", "Black Betty"];
+
+// driver code
+console.log(colors); 
+console.log(horseNames);
+
+// add to each array
+colors.push("white");
+horseNames.push("Sue");
+
+// driver
+console.log(colors); 
+console.log(horseNames);
